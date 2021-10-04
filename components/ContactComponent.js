4036700a29a745caf4e36f61e7b10e7d653d36c7
Daniel Wykes <daniel.j.wykes@alumni.psu.edu>
2021-10-03 23:25:50 -0400

    Workshop: Add About and Contact components and details within

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
-
+import { Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
 
 class Contact extends Component {
 
@@ -11,7 +11,20 @@ class Contact extends Component {
     render() {
         return (
             <ScrollView>
-
+                <Card
+                    title="Contact Information"
+                    wrapperStyle={{margin: 20}}
+                >
+                    <Text>1 Nucamp Way</Text>
+                    <Text>Seattle, WA 98001</Text>
+                    <Text
+                        style={{marginBottom: 10}}
+                    >
+                        U.S.A.
+                    </Text>
+                    <Text>Phone: 1-206-555-1234</Text>
+                    <Text>Email: campsites@nucamp.co</Text>
+                </Card>
             </ScrollView>
         );
         

9a5d22b7681ec48c7c3897eb798a2d3900e16613
Daniel Wykes <daniel.j.wykes@alumni.psu.edu>
2021-10-05 22:02:29 -0400

    Week 2: Add comments to campsite info

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView, FlatList } from 'react-native';
 import { Card } from 'react-native-elements';
 import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
 
 function RenderCampsite({ campsite }) {
-    
-    if(campsite) {
+
+    if (campsite) {
         return (
             <Card
                 featuredTitle={campsite.name}
-                image={require('./images/react-lake.jpg')} 
+                image={require('./images/react-lake.jpg')}
             >
-                <Text style={{margin: 10}} >
+                <Text style={{ margin: 10 }} >
                     {campsite.description}
                 </Text>
             </Card>
@@ -20,12 +21,42 @@ function RenderCampsite({ campsite }) {
     return <View />;
 }
 
+function RenderComments({ comments }) {
+
+    const renderCommentItem = ({ item }) => {
+        return (
+            <View style={{ margin: 10 }}>
+                <Text style={{ fontSize: 14 }}>{item.text}</Text>
+                <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+                <Text style={{ fontSize: 12 }}>{`-- ${item.author}, ${item.date}`}</Text>
+            </View>
+        );
+    };
+
+    if (comments) {
+        return (
+            <Card
+                title='Comments'
+            >
+                <FlatList
+                    data={comments}
+                    renderItem={renderCommentItem}
+                    keyExtractor={item = item.id.toString()}
+                />
+            </Card>
+        );
+    }
+    return <View />
+
+}
+
 class CampsiteInfo extends Component {
-    
+
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES
+            campsites: CAMPSITES,
+            comments: COMMENTS
         }
     }
 
@@ -36,8 +67,14 @@ class CampsiteInfo extends Component {
     render() {
         const campsiteId = this.props.navigation.getParam('campsiteId');
         const campsite = this.state.campsites.filter(campsite => campsite.id === campsiteId)[0];
+        const comments = this.state.comments.filter(comment => comment.campsiteId === campsiteId);
 
-        return <RenderCampsite campsite={campsite} />;
+        return (
+            <ScrollView>
+                <RenderCampsite campsite={campsite} />
+                <RenderComments comments={comments} />
+            </ScrollView>
+        );
     }
 }
 

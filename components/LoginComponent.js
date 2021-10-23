893fa0c7fbd66c51d3e29b4df6fe7f6c452105c7
Daniel Wykes <daniel.j.wykes@alumni.psu.edu>
2021-10-23 10:26:23 -0400

    Week 4: Add option to search for image in gallery when registering, make netInfo an async/await function and save images captured from camera in app

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -3,6 +3,8 @@ import { View, StyleSheet, ScrollView, Image } from 'react-native';
 import { Input, Button, CheckBox, Icon } from 'react-native-elements';
 import * as SecureStore from 'expo-secure-store';
 import * as ImagePicker from 'expo-image-picker';
+import * as ImageManipulator from 'expo-image-manipulator';
+import * as MediaLibrary from 'expo-media-library';
 import * as Permissions from 'expo-permissions';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { baseUrl } from '../shared/baseUrl';
@@ -166,9 +168,45 @@ class RegisterTab extends Component {
 
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({ imageUrl: capturedImage.uri });
+                this.processImage(capturedImage.uri);
             }
         }
+    }
+
+    getImageFromGallery = async () => {
+        const cameraRollPermission = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
+
+        if (cameraRollPermission.status === 'granted') {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1]
+            });
+
+            if (!capturedImage.cancelled) {
+                console.log(capturedImage);
+                this.processImage(capturedImage.uri);
+            }
+        }
+    }
+
+    processImage = async (imgUri) => {
+        const processedImage = await ImageManipulator.manipulateAsync(
+            imgUri,
+            [
+                {
+                    resize: {
+                        height: 400,
+                        width: 400
+                    }
+                }
+            ],
+            { format: ImageManipulator.SaveFormat.PNG }
+        );
+
+        MediaLibrary.saveToLibraryAsync(processedImage.uri)
+
+        console.log(JSON.stringify(processedImage));
+        this.setState({ imageUrl: processedImage.uri });
     }
 
     handleRegister() {
@@ -199,6 +237,10 @@ class RegisterTab extends Component {
                         <Button
                             title='Camera'
                             onPress={this.getImageFromCamera}
+                        />
+                        <Button
+                            title='Gallery'
+                            onPress={this.getImageFromGallery}
                         />
                     </View>
                     <Input
